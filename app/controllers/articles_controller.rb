231eae199fa079bf94ac1a41d4dee65cd38eada6
Thomas West <te.west.08@gmail.com>
2014-09-26 21:39:05 -0600

    completed 5.11 - all debugged

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,13 +15,31 @@ class ArticlesController < ApplicationController
       end
   end
 
+
   def show
       @article = Article.find(params[:id])
     end
 
+
   def index
       @articles = Article.all
   end
+
+
+  def edit
+      @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
 
   private
     def article_params
